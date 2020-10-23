88906954c9f3824fab4449e75f2aea0ba6e3c9ba
Jackson Godwin <jaxjunkmail80@gmail.com>
2020-10-23 01:34:17 -0400

    thurs work

---
+++ ObEx1.js
@@ -0,0 +1,36 @@
+// JavaScript Objects and Constructors:
+
+function Person(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+}
+
+Person.prototype.greet = function(other) {
+    console.log('Hello ' + other.name + ', I am ' + this.name + '!');
+};
+
+//Given the above code do the following:
+
+//-1- Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', 
+//     and phone of '483-485-4948', store it in the variable sonny.
+
+const sonny = new Person("Sonny", "sonny@hotmail.com", "483-485-4948");
+
+
+//-2- Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com',
+//    and phone of '495-586-3456', store it in the variable 'jordan'.
+
+const jordan = new Person("Jordan", "jordan@aol.com", "495-586-3456");
+
+//-3- Have sonny greet jordan using the greet method.
+
+//-4- Have jordan greet sonny using the greet method.
+
+//-5- Write a statement to print the contact info (email and phone) of Sonny.
+
+for ()
+
+
+
+//-6- Write another statement to print the contact info of Jordan.
